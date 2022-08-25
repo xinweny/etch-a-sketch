1f3b12a858da41960929fac695d08e665f55048e
Xin Wen Yap <yapxinwen1608@gmail.com>
2022-08-25 12:05:00 -0400

    Modify cell size specification to change cell width/height while fixing grid dimension

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 function createGrid(dim) {
     const grid = document.querySelector(".grid");
-
-    // Set width and height of grid container based on dimension
-    grid.style.width = `${dim * 20}px`;
-    grid.style.height = grid.style.width;
+    const gridSize = grid.clientWidth;
 
     // Create and insert cells into parent grid div
     for (let i = 0; i < dim ** 2; i++) {
         const cell = document.createElement("div");
         cell.classList.add("cell");
+        cell.style.width = gridSize / dim + "px";
+        cell.style.height = cell.style.width;
+
         grid.appendChild(cell);
     }
+
+    return grid
 }
 
 function main() {
-    createGrid(16);
+    const grid = createGrid(16);
 }
 
 main();
