c0eeba0e818a216e0cd934436f6009fb06cb06dc
Xin Wen Yap <yapxinwen1608@gmail.com>
2022-08-25 11:35:44 -0400

    Add dynamic functionality in determining grid size based on no. of cells

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 function createGrid(dim) {
-    const cells = dim ** 2;
     const grid = document.querySelector(".grid");
 
     // Set width and height of grid container based on dimension
+    grid.style.width = `${dim * 20}px`;
+    grid.style.height = grid.style.width;
+
     // Create and insert cells into parent grid div
-    for (let i = 0; i < cells; i++) {
+    for (let i = 0; i < dim ** 2; i++) {
         const cell = document.createElement("div");
         cell.classList.add("cell");
         grid.appendChild(cell);
     }
-
 }
 
 function main() {
